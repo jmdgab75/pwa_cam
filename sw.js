d54b70277a9c6571efbb916c6951bda5a440e5d4
jmdgab75 <gabrieljmd75@gmail.com>
2023-11-13 13:42:35 -0400

    commit teste 2

--- sw.js
+++ sw.js
@@ -1,56 +1,57 @@
 import { offlineFallback, warmStrategyCache } from 'workbox-recipes';
-import { cacheFirst, StaleWhileRevalidate } from 'workbox-strategies';
+import { CacheFirst, StaleWhileRevalidate } from 'workbox-strategies';
 import { registerRoute, Route } from 'workbox-routing';
 import { CacheableResponsePlugin } from 'workbox-cacheable-response';
-import { Expirationplugin } from 'workbox-expiration' ;
+import { ExpirationPlugin } from 'workbox-expiration';
 
-
-const pageCache = new cacheFirst({
-   cacheName: ' noticias ',
-   plugins: [
-      new CacheableResponsePlugin({
-         statuses: [0, 200],
-      }),
-    new Expirationplugin({
-        maxAgeSeconds: 30 * 24 * 60 * 60,
-    }),
-   ],
-});
-
-
-warmStrategyCache({
-   urls: ['/index.html', '/',
-    "https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@500&display=swap"],
-  strategy: pageCache,
-});
-
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
-
-registerRoute( 
-    ({ request }) => ['style', 'script', 'worker']
-  .includes(request.destination),
-new StaleWhileRevalidate({
-  cacheName: 'asset-cache',
+// configurando o cache
+const pageCache = new CacheFirst({
+  cacheName: 'pwa-cam',
   plugins: [
     new CacheableResponsePlugin({
-      statuses: [O, 200],
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ],
+});
+
+//indicando o cache de página
+warmStrategyCache({
+  urls: ['/index.html', '/'],
+  strategy: pageCache,
+});
+//registrando a rota
+registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+
+
+registerRoute(// configurando cache de assets
+  ({ request }) => ['style', 'script', 'worker']
+    .includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
       }),
     ],
   }),
 );
 
-offlineFallback({ 
- pageFallback: '/offline. html' ,
+offlineFallback({// configurando offline fallback
+  pageFallback: '/offline.html',
 });
 
 const imageRoute = new Route(({ request }) => {
   return request.destination === 'image';
 }, new CacheFirst({
-cacheName: 'images',
-   plugins: [ 
-     new Expirationplugin({
-        maxAgeSeconds: 60 * 60 * 24 * 30,
-     })
+  cacheName: 'images',
+  plugins: [
+    new ExpirationPlugin({
+      maxAgeSeconds: 60 * 60 * 24 * 30,
+    })
   ]
 }));
 registerRoute(imageRoute);
+
